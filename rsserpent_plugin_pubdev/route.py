36cd179d047b2681dbf2d52fa58cee4ed8cd17b4
EkkoG <beijiu572@gmail.com>
2024-05-30 14:59:20 +0800

    chore: format code

--- rsserpent_plugin_pubdev/route.py
+++ rsserpent_plugin_pubdev/route.py
@@ -1,9 +1,9 @@
-from typing import Any, Dict
+from typing import Any
 
 import arrow
-from rsserpent_rev.utils import HTTPClient, cached
 from lxml import html
 
+from rsserpent_rev.utils import HTTPClient, cached
 
 path = "/pubdev/update/{package}"
 
@@ -14,18 +14,18 @@ def get_date(date_str: str) -> arrow.Arrow:
         try:
             date = arrow.get(date_str.strip(), fmt)
             break
-        except Exception as e:
+        except Exception:
             date = arrow.now()
 
     return date
 
-@cached
-async def provider(package: str) -> Dict[str, Any]:
 
+@cached
+async def provider(package: str) -> dict[str, Any]:
     async with HTTPClient() as client:
         changelog_url = f"https://pub.dev/packages/{package}/changelog"
         html_text = (await client.get(changelog_url)).content.decode("utf-8")
-        tree  = html.fromstring(html_text)
+        tree = html.fromstring(html_text)
 
         changelog_entries = tree.xpath("//div[@class='changelog-entry']")
         changelog = {}
@@ -36,19 +36,17 @@ async def provider(package: str) -> Dict[str, Any]:
 
         versions_url = f"https://pub.dev/packages/{package}/versions"
         versions_content = (await client.get(versions_url)).content.decode("utf-8")
-        tree  = html.fromstring(versions_content)
+        tree = html.fromstring(versions_content)
 
         tbody = tree.xpath("//tbody")[0]
         rows = tbody.xpath(".//tr")
-        # <a class="-x-ago" href="" title="May 8, 2024" aria-label="12 days ago" aria-role="button" role="button" data-timestamp="1715192211854">12 days ago</a>
-        # <a class="-x-ago" href="" title="3 months ago" aria-label="3 months ago" aria-role="button" role="button" data-timestamp="1708001900493">Feb 15, 2024</a>
         items = [
             {
                 "title": f"{package} {row.xpath('.//td')[0].text_content()} 更新",
-                "description": changelog.get(row.xpath('.//td')[0].text_content(), ""),
-                "link": "https://pub.dev" + row.xpath('.//td')[0].xpath(".//a")[0].attrib["href"],
+                "description": changelog.get(row.xpath(".//td")[0].text_content(), ""),
+                "link": "https://pub.dev" + row.xpath(".//td")[0].xpath(".//a")[0].attrib["href"],
                 # format a tag's data-timestamp
-                "pub_date": arrow.get(row.xpath('.//td')[3].xpath(".//a")[0].attrib["data-timestamp"], "x"),
+                "pub_date": arrow.get(row.xpath(".//td")[3].xpath(".//a")[0].attrib["data-timestamp"], "x"),
             }
             for row in rows
         ]
@@ -60,7 +58,3 @@ async def provider(package: str) -> Dict[str, Any]:
         "pub_date": items[0]["pub_date"],
         "items": items,
     }
-
-
-
-        
